454f4b06542c6f8dd30cd5406e352cf1f0952685
bszells@gmail.com <bzeslls@gmail.com>
2019-08-11 17:19:53 -0500

    First Commit

---
+++ Tidi/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Tidi
+//
+//  Created by Brad Zellman on 8/11/19.
+//  Copyright © 2019 Brad Zellman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
