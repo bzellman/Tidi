ebd8f30cf01ad46d13b4d0c771489dd5434a865a
bszells@gmail.com <bzeslls@gmail.com>
2019-08-11 22:18:53 -0500

    Basic layout and file structure

---
+++ Tidi/InfoPanelViewController.swift
@@ -0,0 +1,10 @@
+//
+//  InfoPanelViewController.swift
+//  Tidi
+//
+//  Created by Brad Zellman on 8/11/19.
+//  Copyright © 2019 Brad Zellman. All rights reserved.
+//
+
+import Foundation
+import Cocoa
