83b76ec5e0027be59bce5de64353f398e0593d08
bszells@gmail.com <bzeslls@gmail.com>
2019-08-13 21:12:58 -0500

    WIP get static file list in source table view

---
+++ Tidi/SourceTableModel.swift
@@ -0,0 +1,14 @@
+//
+//  SourceTableModel.swift
+//  Tidi
+//
+//  Created by Brad Zellman on 8/13/19.
+//  Copyright © 2019 Brad Zellman. All rights reserved.
+//
+
+import Foundation
+
+class SourceTableModel {
+    var sourceTableFileFolderURL: String = ""
+    var sourceTableFileURLArray: Array<Any> = []
+}
--- Tidi/SourceTableViewController.swift
+++ Tidi/SourceTableViewController.swift
@@ -8,3 +8,58 @@
 
 import Foundation
 import Cocoa
+
+class SourceTableViewController: NSViewController {
+    //need to become dynamic
+    
+    let defaultSourceFolderURL = NSURL(string: "file:///Users/uicentric/Downloads/")
+    
+    
+    var sourceTableFileFolderURL: NSURL = NSURL(string: "file:///Users/uicentric/Downloads/")!
+    var sourceTableFileURLArray: [URL] = []
+    
+    
+    
+    @IBOutlet weak var sourceTableView: NSTableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sourceTableView.delegate = self
+        sourceTableView.dataSource = self
+        
+        sourceTableView.registerForDraggedTypes([.fileURL])
+        sourceTableView.setDraggingSourceOperationMask(.move, forLocal: false)
+        
+    }
+    
+    
+    
+    var filesList = contentsOf(folder: self.sourceTableFileFolderURL)
+    
+    
+    
+    
+}
+
+extension SourceTableViewController: NSTableViewDataSource {
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return self.sourceTableFileURLArray.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let cell = tableView.makeView(withIdentifier: .sourceCellView, owner: self) as! NSTableCellView
+        cell.textField?.stringValue = self.sourceTableFileURLArray[row] as! String
+        return cell
+    }
+
+    
+}
+
+extension SourceTableViewController: NSTableViewDelegate {
+    
+}
+
+extension NSUserInterfaceItemIdentifier {
+    static let sourceCellView = NSUserInterfaceItemIdentifier("sourceCellView")
+}
