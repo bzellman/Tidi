f3449f3a9b3c1b9d5489a141561ab5f1273e1e22
bszells@gmail.com <bzeslls@gmail.com>
2020-01-29 23:49:01 -0600

    clean up BorderedVire

--- Tidi/Custom Views/BorderedView.swift
+++ Tidi/Custom Views/BorderedView.swift
@@ -12,13 +12,11 @@ class BorderedView: NSView {
     
 
     override func draw(_ dirtyRect: NSRect) {
-   
+        super.draw(dirtyRect)
+        
         let path : NSBezierPath = NSBezierPath(roundedRect: self.bounds, xRadius: 10.0, yRadius: 10.0)
-        let cgpath : CGPath = CGPath(roundedRect: NSRectToCGRect(self.bounds), cornerWidth: 10.0, cornerHeight: 10.0, transform: nil)
         path.addClip()
         
-        super.draw(dirtyRect)
-
         let dashHeight: CGFloat = 2
         let dashLength: CGFloat = 6
         let dashColor: NSColor = .lightGray
@@ -30,7 +28,8 @@ class BorderedView: NSView {
         currentContext.setStrokeColor(dashColor.cgColor)
 
         // draw the dashed path
-        currentContext.addPath(cgpath)
+        let cgPath : CGPath = CGPath(roundedRect: NSRectToCGRect(self.bounds), cornerWidth: 10.0, cornerHeight: 10.0, transform: nil)
+        currentContext.addPath(cgPath)
         currentContext.strokePath()
     }
     
